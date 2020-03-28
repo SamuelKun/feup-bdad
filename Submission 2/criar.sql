40abda2e21bd68380654c0d25625f8d0767b6264
Samuel <diogosamuelfernandes@outlook.com>
2020-03-28 19:01:43 +0000

    Update tabela sql

--- Submission 2/criar.sql
+++ Submission 2/criar.sql
@@ -5,23 +5,95 @@ PRAGMA foreign_keys = on;
 
 DROP TABLE IF EXISTS Pessoa;
 CREATE TABLE Pessoa (
-  id             INTEGER PRIMARY KEY,
-  nome           VARCHAR(255) NOT NULL,
-  dataNascimento DATE NOT NULL,
-  codPostal      INTEGER,
-  morada         STRING NOT NULL
+  id                          INTEGER,
+  nome                        VARCHAR(255),
+  dataNascimento              INTEGER,
+  codPostal                   INTEGER,
+  morada                      INTEGER,
+  PRIMARY KEY (id)
+
 );
 
 DROP TABLE IF EXISTS Artista;
 CREATE TABLE Artista (
-  id              INTEGER REFERENCES Pessoa (id) PRIMARY KEY,
-  inicioCarreira  INTEGER
+  id                          INTEGER REFERENCES Pessoa (id),
+  inicioCarreira              INTEGER
+  PRIMARY KEY (id)
 );
 
 DROP TABLE IF EXISTS Utilizador;
 CREATE TABLE Utilizador (
-  id             INTEGER REFERENCES Pessoa (id) PRIMARY KEY,
-  email          VARCHAR(255) NOT NULL,
-  username       VARCHAR(255) NOT NULL,
-  password       VARCHAR(255) NOT NULL UNIQUE
+  id                         INTEGER REFERENCES Pessoa (id),
+  email                      INTEGER,
+  username                   INTEGER,
+  password                   INTEGER
 );
+
+DROP TABLE IF EXISTS Papel;
+CREATE TABLE Papel (
+  idPapel                    INTEGER,
+  atividade                  INTEGER
+);
+
+DROP TABLE IF EXISTS EntidadeMusical;
+CREATE TABLE EntidadeMusical (
+  idEntidadeMusical          INTEGER,
+  nomeArtistico              INTEGER,
+  imagem                     INTEGER,
+  dataFundacao               INTEGER,
+  descricao                  INTEGER
+);
+
+DROP TABLE IF EXISTS Album;
+CREATE TABLE Album (
+  idAlbum                    INTEGER,
+  nome                       INTEGER,
+  capa                       INTEGER,
+  anoLancamento              INTEGER
+);
+
+DROP TABLE IF EXISTS Musica;
+CREATE TABLE Musica (
+  idMusica                   INTEGER,
+  album                      REFERENCES Album(idAlbum)
+  nome                       INTEGER,
+  duracao                    INTEGER
+);
+
+DROP TABLE IF EXISTS EstiloMusical;
+CREATE TABLE EstiloMusical (
+  idEstiloMusical            INTEGER,
+  nome                       INTEGER,
+  duracao                    INTEGER
+);
+
+DROP TABLE IF EXISTS Playlist;
+CREATE TABLE Playlist (
+  idPlaylist                 INTEGER,
+  criador                    INTEGER REFERENCES  Utilizador(id),
+  nome                       INTEGER,
+  imagem                     INTEGER,
+  dataCriacao                INTEGER,
+  descricao                  INTEGER,
+  privada                    INTEGER
+);
+
+DROP TABLE IF EXISTS Sessao;
+CREATE TABLE Sessao (
+  idSessao                   INTEGER,
+  dataInicio                 INTEGER
+);
+
+DROP TABLE IF EXISTS Adicionada;
+CREATE TABLE Adicionada (
+  utilizador                 INTEGER REFERENCES Utilizador(id)
+);
+
+DROP TABLE IF EXISTS TempoOuvido;
+CREATE TABLE TempoOuvido (
+  musica                     INTEGER REFERENCES Musica(idMusica)
+  sessao                     INTEGER REFERENCES Sessao(idSessao)
+  duracao                    INTEGER
+);
+
+PRAGMA foreign_keys = on;
