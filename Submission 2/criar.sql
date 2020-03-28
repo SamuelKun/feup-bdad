fd27793fa79228c9f20b1483a5395c832288e729
Hugomguima <50011557+Hugomguima@users.noreply.github.com>
2020-03-28 21:26:43 +0000

    Update criar.sql

--- Submission 2/criar.sql
+++ Submission 2/criar.sql
@@ -7,9 +7,9 @@ DROP TABLE IF EXISTS Pessoa;
 CREATE TABLE Pessoa (
   id                          INTEGER,
   nome                        VARCHAR(255),
-  dataNascimento              INTEGER,
-  codPostal                   INTEGER,
-  morada                      INTEGER,
+  dataNascimento              VARCHAR(255),
+  codPostal                   VARCHAR(255),
+  morada                      VARCHAR(255)
   PRIMARY KEY (id)
 
 );
@@ -17,71 +17,71 @@ CREATE TABLE Pessoa (
 DROP TABLE IF EXISTS Artista;
 CREATE TABLE Artista (
   id                          INTEGER REFERENCES Pessoa (id),
-  inicioCarreira              INTEGER
+  inicioCarreira              VARCHAR(255),
   PRIMARY KEY (id)
 );
 
 DROP TABLE IF EXISTS Utilizador;
 CREATE TABLE Utilizador (
   id                         INTEGER REFERENCES Pessoa (id),
-  email                      INTEGER,
-  username                   INTEGER,
-  password                   INTEGER
+  email                      VARCHAR(255),
+  username                   VARCHAR(255),
+  password                   VARCHAR(255)
 );
 
 DROP TABLE IF EXISTS Papel;
 CREATE TABLE Papel (
   idPapel                    INTEGER,
-  atividade                  INTEGER
+  atividade                  VARCHAR(255)
 );
 
 DROP TABLE IF EXISTS EntidadeMusical;
 CREATE TABLE EntidadeMusical (
   idEntidadeMusical          INTEGER,
-  nomeArtistico              INTEGER,
-  imagem                     INTEGER,
-  dataFundacao               INTEGER,
-  descricao                  INTEGER
+  nomeArtistico              VARCHAR(255),
+  imagem                     VARCHAR(255),
+  dataFundacao               VARCHAR(255),
+  descricao                  VARCHAR(255)
 );
 
 DROP TABLE IF EXISTS Album;
 CREATE TABLE Album (
   idAlbum                    INTEGER,
-  nome                       INTEGER,
-  capa                       INTEGER,
-  anoLancamento              INTEGER
+  nome                       VARCHAR(255),
+  capa                       VARCHAR(255),
+  anoLancamento              VARCHAR(255)
 );
 
 DROP TABLE IF EXISTS Musica;
 CREATE TABLE Musica (
   idMusica                   INTEGER,
   album                      REFERENCES Album(idAlbum)
-  nome                       INTEGER,
-  duracao                    INTEGER
+  nome                       VARCHAR(255),
+  duracao                    VARCHAR(255)
 );
 
 DROP TABLE IF EXISTS EstiloMusical;
 CREATE TABLE EstiloMusical (
   idEstiloMusical            INTEGER,
-  nome                       INTEGER,
-  duracao                    INTEGER
+  nome                       VARCHAR(255),
+  duracao                    VARCHAR(255)
 );
 
 DROP TABLE IF EXISTS Playlist;
 CREATE TABLE Playlist (
   idPlaylist                 INTEGER,
   criador                    INTEGER REFERENCES  Utilizador(id),
-  nome                       INTEGER,
-  imagem                     INTEGER,
-  dataCriacao                INTEGER,
-  descricao                  INTEGER,
-  privada                    INTEGER
+  nome                       VARCHAR(255),
+  imagem                     VARCHAR(255),
+  dataCriacao                VARCHAR(255),
+  descricao                  VARCHAR(255),
+  privada                    VARCHAR(255),
 );
 
 DROP TABLE IF EXISTS Sessao;
 CREATE TABLE Sessao (
   idSessao                   INTEGER,
-  dataInicio                 INTEGER
+  dataInicio                 VARCHAR(255)
 );
 
 DROP TABLE IF EXISTS Adicionada;
