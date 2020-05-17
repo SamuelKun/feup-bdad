91678d29ffb53b265ace0009cb4006271bcad97a
Hugomguima <50011557+Hugomguima@users.noreply.github.com>
2020-05-17 20:11:08 +0100

    Files added

---
+++ Projeto/Sumbission 3/src/Gatilho1_verifica.sql
@@ -0,0 +1,8 @@
+Select * from TempoOuvido
+Order By idSessao
+Limit 5;
+Insert into TempoOuvido Values (1,1,12);
+Select * from TempoOuvido
+Order By idSessao
+Limit 5;
+.print "Duração do primeiro tuplo incrementou por 12 em vez de adicionar um novo tuplo"
--- Projeto/Sumbission 3/src/run.sql
+++ Projeto/Sumbission 3/src/run.sql
@@ -1,2 +1,3 @@
 .read criar.sql
 .read povoar.sql
+.read Gatilho1_adiciona.sql
