c9ae5de9a05dbe11b8bdfce8911144410e85077f
PJscp16 <50209613+PJscp16@users.noreply.github.com>
2020-05-20 19:54:26 +0100

    Query 10 (temporary)

--- Projeto/Submission 3/src/int10.sql
+++ Projeto/Submission 3/src/int10.sql
@@ -1,3 +1,8 @@
 .mode	columns
 .headers	on
 .nullvalue	NULL
+SELECT * FROM
+(SELECT idArtista AS idPessoa, idPapel FROM Desempenha NATURAL JOIN Artista WHERE idPapel = 1)
+NATURAL JOIN Pessoa
+WHERE dataNascimento >= "1970-01-01" AND dataNascimento <= "1980-01-01"
+ORDER BY dataNascimento;
