e5ac7a1d50de19fba2ac7e34f3cab4801c4855bd
Hugomguima <50011557+Hugomguima@users.noreply.github.com>
2020-05-22 20:33:53 +0100

    QUERY DA STORA FEITA

---
+++ Projeto/Submission 3/src/test.sql
@@ -0,0 +1,20 @@
+insert into album
+values (14,'short','https://fakeLink',2020);
+
+insert into Musica
+values(150,14,'Preço Certo',30);
+
+insert into Musica
+values(151,14,'Quim das remisturas',45);
+
+insert into entidadeMusical
+values (12,'Hugo Boss','https://eauDeParfum',2020,'Melhor artista de sempre');
+
+insert into compoe
+values (12,14);
+
+insert into tempoOuvido
+values (1,150,60);
+
+insert into tempoOuvido
+values (1,151,67);
