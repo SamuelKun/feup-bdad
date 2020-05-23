2794794b1ee31b78d81dfa79697bb94a277882e7
Hugomguima <50011557+Hugomguima@users.noreply.github.com>
2020-05-23 19:54:09 +0100

    joined twice xDD

--- Projeto/Submission 3/src/int10.sql
+++ Projeto/Submission 3/src/int10.sql
@@ -40,7 +40,7 @@ From
             From UtilizadorSessao natural join tempoOuvido
             Group by idUtilizador,idMusica
         )
-        natural join Musica natural join Utilizador join
+        natural join Utilizador join
         album using (idAlbum)
         where tOuvido > duracao
     )
